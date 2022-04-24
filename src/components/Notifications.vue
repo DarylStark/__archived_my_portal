d7a945868f7be7b90e5f8fa2962b66c700d6d884
Daryl Stark <daryl@dstark.nl>
2022-04-24 21:42:50 +0200

    Added a notification button

---
+++ src/components/Notifications.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="notifications">
+        <i class="fas fa-bell"></i>
+    </div>
+</template>
+
+<script>
+// TODO: Make this component work
+export default {
+    name: 'Notifications',
+};
+</script>
--- src/layout/Feeter.vue
+++ src/layout/Feeter.vue
@@ -2,15 +2,17 @@
     <div class="feeter">
         <ProfileSwitcher></ProfileSwitcher>
         <Spacer></Spacer>
+        <Notifications></Notifications>
     </div>
 </template>
 
 <script>
 import Spacer from '../components/Spacer.vue';
 import ProfileSwitcher from '../components/ProfileSwitcher.vue';
+import Notifications from '../components/Notifications.vue';
 
 export default {
     name: 'Feeter',
-    components: { Spacer, ProfileSwitcher },
+    components: { Spacer, ProfileSwitcher, Notifications },
 };
 </script>
