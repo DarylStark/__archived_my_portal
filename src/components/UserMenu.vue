3c78fdc4a169fe99c42556dd4d99a30dada523ff
Daryl Stark <daryl@dstark.nl>
2022-04-24 21:07:08 +0200

    Restyled the header

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="usermenu">
-    <div class="button">
-      <div
-        v-if="
-          $store.state.user_session.session.account.fullname &&
-          $store.state.ui.device_type != 'phone'
-        "
-      >
-        {{ $store.state.user_session.session.account.fullname }}
-      </div>
-      <i class="fas fa-user-circle"></i>
+    <div v-bind:class="['usermenu', { active: active }]">
+        <div class="button" v-on:click="active = !active">
+            <div
+                v-if="
+                    $store.state.user_session.session.account.fullname &&
+                    $store.state.ui.device_type != 'phone'
+                "
+            >
+                {{ $store.state.user_session.session.account.fullname }}
+            </div>
+            <i class="fas fa-user-circle"></i>
+        </div>
+        <div class="menu">
+            <UserMenuItem icon="fa-cog">Settings</UserMenuItem>
+            <UserMenuItem icon="fa-adjust" v-on:click="next_theme"
+                >Next theme</UserMenuItem
+            >
+            <UserMenuItem icon="fa-edit">Rename session</UserMenuItem>
+            <UserMenuItem icon="fa-sign-out-alt" v-on:click="logout"
+                >Logout</UserMenuItem
+            >
+        </div>
     </div>
-    <div class="menu">
-      <UserMenuItem icon="fa-cog">Settings</UserMenuItem>
-      <UserMenuItem icon="fa-adjust" v-on:click="next_theme"
-        >Next theme</UserMenuItem
-      >
-      <UserMenuItem icon="fa-edit">Rename session</UserMenuItem>
-      <UserMenuItem icon="fa-sign-out-alt" v-on:click="logout"
-        >Logout</UserMenuItem
-      >
-    </div>
-  </div>
 </template>
 
 <script>
@@ -29,15 +29,20 @@ import Ganymede from '../mgm/ganymede.js';
 import UserMenuItem from './UserMenuItem.vue';
 
 export default {
-  name: 'UserMenu',
-  components: { UserMenuItem },
-  methods: {
-    next_theme: () => {
-      Ganymede.commands.execute('theme.next');
+    name: 'UserMenu',
+    components: { UserMenuItem },
+    data: function () {
+        return {
+            active: false,
+        };
     },
-    logout: () => {
-      Ganymede.commands.execute('userssion.logout');
+    methods: {
+        next_theme: () => {
+            Ganymede.commands.execute('theme.next');
+        },
+        logout: () => {
+            Ganymede.commands.execute('userssion.logout');
+        },
     },
-  },
 };
 </script>
