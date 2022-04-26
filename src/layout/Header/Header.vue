4d759e439069d764c4c61991b5b23d016ddf7e76
Daryl Stark <daryl@dstark.nl>
2022-04-26 10:49:10 +0200

    Created a toggle for the Sidebar

--- src/layout/Header/Header.vue
+++ src/layout/Header/Header.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="header">
-    <MenuToggle></MenuToggle>
-    <Spacer></Spacer>
-    <UserMenu></UserMenu>
-  </div>
+    <div class="header">
+        <MenuToggle></MenuToggle>
+        <Spacer></Spacer>
+        <SidebarToggle></SidebarToggle>
+        <UserMenu></UserMenu>
+    </div>
 </template>
 
 <script>
 import MenuToggle from './MenuToggle.vue';
 import Spacer from '../../components/Spacer.vue';
 import UserMenu from './UserMenu.vue';
+import SidebarToggle from './SidebarToggle.vue';
 
 export default {
-  name: 'Header',
-  components: { MenuToggle, Spacer, UserMenu },
+    name: 'Header',
+    components: { MenuToggle, Spacer, UserMenu, SidebarToggle },
 };
 </script>
---
+++ src/layout/Header/SidebarToggle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="sidebartoggle">
+        <i
+            class="fas fa-list-ul"
+            v-on:click="toggle_sidebar"
+            v-bind:class="{ hidden: !sidebar_is_visible }"
+        ></i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SidebarToggle',
+    computed: {
+        sidebar_is_visible() {
+            return this.$store.state.ui.sidebar_visible;
+        },
+    },
+    methods: {
+        toggle_sidebar() {
+            this.$store.commit('sidebar_visible_toggle');
+        },
+    },
+};
+</script>
