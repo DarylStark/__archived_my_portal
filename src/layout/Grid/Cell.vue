5af26ba5708340b4b58083cef79490ce7e2a8b06
Daryl Stark <daryl@dstark.nl>
2022-04-30 20:26:38 +0200

    Created a working, responsive `Grid` for the pages

---
+++ src/layout/Grid/Cell.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-bind:class="class">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cell',
+    computed: {
+        class() {
+            let classlist = [this.type];
+            // Set the column count
+            if (this.cols >= 0 && this.cols <= 12) {
+                classlist = [...classlist, 'cols-' + this.cols];
+            }
+
+            // Specific for desktop
+            if (this.cols_desktop) {
+                classlist = [...classlist, 'cols-desktop-' + this.cols_desktop];
+            }
+
+            // Specific for tablet
+            if (this.cols_tablet) {
+                classlist = [...classlist, 'cols-tablet-' + this.cols_tablet];
+            }
+
+            // Specific for phone
+            if (this.cols_phone) {
+                classlist = [...classlist, 'cols-phone-' + this.cols_phone];
+            }
+
+            // Set the hiders
+            classlist = [this.hide_desktop ? 'hide-desktop' : '', ...classlist];
+            classlist = [this.hide_tablet ? 'hide-tablet' : '', ...classlist];
+            classlist = [this.hide_phone ? 'hide-phone' : '', ...classlist];
+
+            // Return the created list
+            return classlist;
+        },
+    },
+    props: {
+        cols: { type: Number, default: undefined },
+        cols_desktop: { type: Number, default: undefined },
+        cols_tablet: { type: Number, default: undefined },
+        cols_phone: { type: Number, default: undefined },
+        hide_desktop: { type: Boolean, default: false },
+        hide_tablet: { type: Boolean, default: false },
+        hide_phone: { type: Boolean, default: false },
+        type: { type: String, default: 'cell' },
+    },
+};
+</script>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,10 +1,17 @@
 <template>
-    <p>Settings</p>
+    <Grid> </Grid>
 </template>
 
 <script>
+import Grid from '../layout/Grid/Grid';
+import Cell from '../layout/Grid/Cell';
+
 export default {
     name: 'Settings',
+    components: {
+        Grid,
+        Cell,
+    },
     created() {
         this.$store.commit('sidebar_available_set', false);
     },
