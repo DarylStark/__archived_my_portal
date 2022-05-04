07d75c04ff8caa632cbad86045b21b9d1c03d116
Daryl Stark <daryl@dstark.nl>
2022-05-04 19:25:03 +0200

    Created a Toggle component

---
+++ src/components/Toggle.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+        v-bind:class="['toggle', { checked: value }]"
+        v-on:click="toggle_click"
+    >
+        <div class="inner"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Toggle',
+    props: {
+        toggled: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        toc: {
+            type: Boolean,
+            required: false,
+            default: true,
+        },
+    },
+    data() {
+        return {
+            value: this.checked,
+        };
+    },
+    emits: ['changed'],
+    methods: {
+        toggle_click() {
+            if (this.toc) {
+                this.toggle();
+            }
+        },
+        toggle() {
+            this.value = !this.value;
+            this.$emit('changed');
+        },
+    },
+};
+</script>
