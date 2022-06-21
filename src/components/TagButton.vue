abea18c2379d63e002a189fdda85534731d45338
Daryl Stark <daryl@dstark.nl>
2022-06-21 15:33:38 +0200

    Updated the icon for deleting a tag from the dashboard

--- src/components/TagButton.vue
+++ src/components/TagButton.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="tagbutton" v-bind:style="style">
         <div class="text" v-on:click="go_to_tag">{{ tag.title }}</div>
-        <div
-            class="delete"
-            v-if="delete_method"
-            v-on:click="delete_method(tag)"
-        >
-            <i v-bind:class="tag_class"></i>
+        <div class="delete" v-if="!tag.loading" v-on:click="delete_method(tag)">
+            <i class="fa-solid fa-xmark"></i>
+        </div>
+        <div v-if="tag.loading" class="delete">
+            <i class="fa-solid fa-spinner spin"></i>
         </div>
     </div>
 </template>
@@ -28,15 +27,6 @@ export default {
                 'border-color': `#${this.tag.color}`,
                 background: `#${this.tag.color}90`,
             };
-        },
-        tag_class() {
-            return [
-                'fa-solid',
-                'fa-xmark',
-                {
-                    spin: this.tag.loading,
-                },
-            ];
         },
     },
     methods: {
